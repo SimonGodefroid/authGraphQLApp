fc6404fc3e846220c0bd2b3a9cb261f59b14cf0c
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-07 16:32:07 +0100

    connected the currentuser query to the login form and added navigation

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,10 +3,20 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router';
 class LoginForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { errors: [] };
+	}
+	componentWillUpdate(nextProps) {
+		// this.props // the old, current set of props
+		// nextProps // the next set of props that will be in place when the component rerenders
+		console.log(this.props, nextProps);
+		if (!this.props.data.user && nextProps.data.user) {
+			// redirect to dashboard !!!
+			hashHistory.push('/dashboard');
+		}
 	}
 	onSubmit({ email, password }) {
 		this.props
@@ -29,4 +39,4 @@ class LoginForm extends Component {
 	}
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(graphql(mutation)(LoginForm));
