77cfa2943a99c13b01d3d834c730dc82078c9a24
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-07 11:52:48 +0100

    added signup, login, logout mutations

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -14,7 +14,25 @@ const mutation = new GraphQLObjectType({
 			},
 			// request is the request object coming from express
 			resolve(parentValue, { email, password }, req) {
-				AuthService.signup({ email, password, req });
+				return AuthService.signup({ email, password, req });
+			}
+		},
+		logout: {
+			type: UserType,
+			resolve(parentValue, args, req) {
+				const { user } = req;
+				req.logout();
+				return user;
+			}
+		},
+		login: {
+			type: UserType,
+			args: {
+				email: { type: GraphQLString },
+				password: { type: GraphQLString }
+			},
+			resolve(parentValue, { email, password }, req) {
+				return AuthService.login({ email, password, req });
 			}
 		}
 	}
