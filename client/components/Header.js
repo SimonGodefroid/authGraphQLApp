1589b59e4914af96c9050c0aa6fbbe5106ef5cc6
SimonGodefroid <simon.godefroid@gmail.com>
2017-12-07 12:47:35 +0100

    added some logic to the header component

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 class Header extends Component {
+	renderButtons() {
+		const { loading, user } = this.props.data;
+		if (loading) {
+			return <div />;
+		}
+		if (user) {
+			return <div>Logout</div>;
+		} else {
+			return <div>You're not signed in.</div>;
+		}
+	}
 	render() {
-		return <div>Header</div>;
+		return (
+			<nav>
+				<div className={`nav-wrapper`}>{this.renderButtons()}</div>
+			</nav>
+		);
 	}
 }
 export default graphql(query)(Header);
